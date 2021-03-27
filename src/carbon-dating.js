1a57e0c3ecf33a03fa4e53aa0fb4cc94fc04a898
github-prozhenik <djfiers@gmail.com>
2021-03-27 17:24:04 +0300

    fix: error fixed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,11 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY= 15;
+const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof(Number(sampleActivity)) != Number){
+  const sampleNum = Number.parseFloat(sampleActivity);
+  if(typeof sampleActivity !== 'string' || isNaN(sampleNum) || sampleNum <= 0 || sampleNum > MODERN_ACTIVITY) {
     return false;
-  }
-  return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)));
-}
+  };
+  const A0A = MODERN_ACTIVITY / sampleNum;
+  const k = Math.log(2) / HALF_LIFE_PERIOD;
+  const t = Math.log(A0A) / k;
+  return Math.ceil(t);
+};
