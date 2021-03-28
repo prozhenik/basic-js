8dbda7d068204794a67693b8fa65f24d97125c44
github-prozhenik <djfiers@gmail.com>
2021-03-28 16:07:24 +0300

    feat: dream-team completed

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let secretname = [];
+  if (!Array.isArray(members)) return false;
+  members.forEach((member) => {
+    if (typeof member == "string") {
+      secretname.push(member.trim()[0].toUpperCase());
+    }
+    return secretname;
+  });
+  return secretname.length ? secretname.sort().join("") : false;
 };
