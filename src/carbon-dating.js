a35019cb131c41ecf683b7cb7278ae7734fef7cb
github-prozhenik <djfiers@gmail.com>
2021-10-05 00:17:43 +0300

    feat: migrate to ESM

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,32 +1,9 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-const MODERN_ACTIVITY= 15;
-const HALF_LIFE_PERIOD= 5730;
-
-/**
- * Determine the age of archeological find by using
- * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
- * @return {Number | Boolean} calculated age in years or false
- * in case of incorrect sampleActivity
- *
- * @example
- * 
- * dateSample('1') => 22387
- * dateSample('WOOT!') => false
- *
- */
- export default function dateSample(sampleActivity) {
-  if (
-    !sampleActivity ||
-    typeof sampleActivity !== 'string' ||
-    isNaN(sampleActivity) ||
-    sampleActivity > MODERN_ACTIVITY ||
-    sampleActivity <= 0
-  ) {
-    return false;
-  }
-
-  return Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD));
+export default function dateSample(sampleActivity) {
+  if ((typeof sampleActivity !== 'string') || (Number.isNaN(Number(sampleActivity)))) return false;
+  let value = Number(sampleActivity);
+  if (value <= 0 || value >= MODERN_ACTIVITY) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY / value) / 0.693 * HALF_LIFE_PERIOD);
 }
