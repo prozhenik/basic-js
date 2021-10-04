5af4672a2f0421a2a5bc261d51f6046fcbb2ed19
github-prozhenik <djfiers@gmail.com>
2021-10-05 00:53:25 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,27 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+  if (options.hasOwnProperty('addition')) {
+    if (options.hasOwnProperty('additionRepeatTimes')) {
+      const repeatedAddition = Array(options.additionRepeatTimes).fill(String(options.addition));
+      if (options.hasOwnProperty('additionSeparator')) {
+        str += repeatedAddition.join(options.additionSeparator);
+      } else {
+        str += repeatedAddition.join('|');
+      }
+    } else {
+      str += options.addition;
+    }
+  }
+
+  if (options.hasOwnProperty('repeatTimes')) {
+    const repeatedStr = Array(options.repeatTimes).fill(str);
+    if (options.hasOwnProperty('separator')) {
+      return repeatedStr.join(options.separator);
+    } else {
+      return repeatedStr.join('+');
+    }
+  }
+  return str;
 }
