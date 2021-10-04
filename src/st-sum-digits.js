7472941d2f0b39f04988209de25d34b15ddafe12
github-prozhenik <djfiers@gmail.com>
2021-10-05 01:07:55 +0300

    feat: implement st-sum-digits

--- src/st-sum-digits.js
+++ src/st-sum-digits.js
@@ -12,7 +12,10 @@ import { NotImplementedError } from '../extensions/index.js';
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-export default function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSumOfDigits(n) {
+  let sum = String(n).split('').reduce((acc, el) => acc + Number(el), 0);
+  if (String(sum).length > 1) {
+    sum = getSumOfDigits(sum);
+  }
+  return sum;
 }
