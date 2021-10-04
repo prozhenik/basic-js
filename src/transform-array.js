b3c1869b43175ce182be8d1ae655d2273d682ec7
github-prozhenik <djfiers@gmail.com>
2021-10-05 01:09:39 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,58 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const commands = {
+    '--discard-next': (result, arr, index) => {
+      return {
+        result: result,
+        i: index + 1
+      };
+    },
+    '--discard-prev': (result, arr, index) => {
+      if (arr[index - 2] !== '--discard-next') {
+        result.pop();
+      }
+
+      return {
+        result: result,
+        i: index
+      };
+    },
+    '--double-next': (result, arr, index) => {
+      if (index < arr.length - 1) {
+        result.push(arr[index + 1]);
+      }
+      return {
+        result: result,
+        i: index
+      };
+    },
+    '--double-prev': (result, arr, index) => {
+      if (index >= 1 && arr[index - 2] !== '--discard-next') {
+        result.push(arr[index - 1]);
+      }
+
+      return {
+        result: result,
+        i: index
+      };
+    },
+  }
+
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (commands.hasOwnProperty(arr[i])) {
+      ({ result, i } = commands[arr[i]](result, arr, i));
+    }
+    else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
